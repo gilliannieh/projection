4d23df7a000f0cb57b2f98355ffdad41f6397938
elsalama <125615595+elsalama@users.noreply.github.com>
2025-04-28 11:53:02 -0500

    API key loaded
    loading the API key and setting up react.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
+import config from './config';
 
 function App() {
+  // You can use config.apiKey here or in any API calls
+  console.log('API Key loaded:', config.apiKey ? 'Yes' : 'No');
+
   return (
     <div className="App">
       <Sidebar />
